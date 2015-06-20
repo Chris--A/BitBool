8246c812d167e58200b27e33053be37ec052880a
Chris--A <chris@genx.biz>
2015-06-20 20:18:46 +1000

    Update BitBool
    A tidy up and nicer naming of internals.

--- BitBool.h
+++ BitBool.h
@@ -1,26 +1,26 @@
 /* 
-	The MIT License (MIT)
+    The MIT License (MIT)
 
-	Copyright (c) 2012 Christopher Andrews
-	http://arduino.land/Code/BitBool/
-	
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
+    Copyright (c) 2012 Christopher Andrews
+    http://arduino.land/Code/BitBool/
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE. 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE. 
 */
 
 #ifndef HEADER_BITBOOL
@@ -29,31 +29,33 @@
   #include <stdint.h>
   #include <stddef.h>
 
-  template< size_t _Count, bool _MSBLeft = false >
+template< size_t _Count, bool _Reverse = false >
     class BitBool{
-      protected:
+    protected:
         struct BitRef{
-        
-          BitRef( uint8_t &u_DataRef, const uint8_t u_Idx ) : u_Data( u_DataRef ), u_Index( _MSBLeft ? ( 0x80 >> u_Idx ) : ( 0x1 << u_Idx ) ) { return; }
 
-          operator const bool() const { return this->u_Data & this->u_Index; }
-          
-          const bool operator =( const BitRef &b_Copy ) const { return *this = ( const bool ) b_Copy; }
+            BitRef( uint8_t &dataRef, const uint8_t idx ) : 
+                data( dataRef ), 
+                index( _Reverse ? ( 0x80 >> idx ) : ( 0x1 << idx ) ) 
+                { return; }
 
-          const bool operator =( const bool &b_Copy ) const
-            {
-              if( b_Copy )  this->u_Data |= this->u_Index;
-              else          this->u_Data &= ~this->u_Index;
-              return b_Copy;
-            }      
-            
-          uint8_t &u_Data;
-          uint8_t const u_Index;
+            operator const bool() const { return data & index; }
+
+            const bool operator =( const BitRef &copy ) const { return *this = ( const bool ) copy; }
+
+            const bool operator =( const bool &copy ) const {
+                if( copy ) data |= index;
+                else       data &= ~index;
+                return copy;
+            }
+
+            uint8_t &data;
+            uint8_t const index;
         };  
-      public:
-        enum{ BitCount = _Count, ByteCount = ( BitCount / 0x8 ) + ( ( BitCount % 0x8 ) ? 0x1 : 0x0 ) };
+    public:
+        enum{ bitCount = _Count, byteCount = ( bitCount / 0x8 ) + ( ( bitCount % 0x8 ) ? 0x1 : 0x0 ) };
 
-        BitRef operator []( const uint16_t i_Index ) { return BitRef( this->Data[ i_Index >> 0x3 ], i_Index & 0x7 ); }  
-        uint8_t Data[ ByteCount ];
-  };
+        BitRef operator []( const uint16_t index ) { return BitRef( data[ index >> 0x3 ], index & 0x7 ); }  
+        uint8_t data[ byteCount ];
+};
 #endif
